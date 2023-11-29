49b52c66dee3f958e7731b22b531a8d5bc9f69d3
Danar <priyambodo02@gmail.com>
2023-11-29 14:19:30 +0700

    Up Project

---
+++ app/Views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <h2><br>Danar Sodik Priyambodo</h2>
+    <h2>5025211145</h2>
+</body>
+</html>
